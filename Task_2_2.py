fbb5f26c2aea7b42a554dbd8f310fdd62c3b4737
ChernovIvan86 <4ern86@bk.ru>
2023-09-23 00:26:51 +0300

    Task_2_2.py

---
+++ Task_2_2.py
@@ -0,0 +1,25 @@
+# Введите список lst, состоящий из чисел.
+# Найдите и напечатайте наименьшее число из списка lst.
+# Не используйте min, sort, sorted.
+from random import randint
+lst=[]
+dct={}
+dct_min={}
+
+for i in range(10):
+    lst.append(randint(-10, 10))
+print(lst)
+
+dct={'min':lst[0]}
+
+for i in range(0, len(lst)-1):
+    if lst[i] <= dct['min']: dct['min']=lst[i]
+print(*dct, end='')
+print(f' in lst = {dct["min"]}')
+
+# Найдите и напечатайте ВСЕ наименьшие числа из списка lst.
+for i in range(0, len(lst)-1):
+    if lst[i] == dct['min']: dct_min['index '+str(i)]=lst[i]
+print(dct_min)
+
+
