723c3f5d0e31819930351be4d6afba4a9585c4a4
ChernovIvan86 <4ern86@bk.ru>
2023-09-23 23:37:51 +0300

    Task_2_2.py

--- Task_2_2.py
+++ Task_2_2.py
@@ -6,19 +6,22 @@ lst=[]
 dct={}
 dct_min={}
 
+'''
 for i in range(10):
     lst.append(randint(-10, 10))
+'''
+lst = [0, 1, 0, 2, 2, 0, 3, 3, 3, 0]
 print(lst)
 
 dct={'min':lst[0]}
 
-for i in range(0, len(lst)-1):
+for i in range(0, len(lst)):
     if lst[i] <= dct['min']: dct['min']=lst[i]
 print(*dct, end='')
 print(f' in lst = {dct["min"]}')
 
 # Найдите и напечатайте ВСЕ наименьшие числа из списка lst.
-for i in range(0, len(lst)-1):
+for i in range(0, len(lst)):
     if lst[i] == dct['min']: dct_min['index '+str(i)]=lst[i]
 print(dct_min)
 
